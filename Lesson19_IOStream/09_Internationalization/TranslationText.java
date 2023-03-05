1f12a03d0667fb158e4a4571aa941cb0be79c12e
Ann Barcomb <ann.barcomb@ucalgary.ca>
2023-03-05 15:00:47 -0700

    Week 9

---
+++ Lesson19_IOStream/09_Internationalization/TranslationText.java
@@ -0,0 +1,34 @@
+package edu.ucalgary.oop;
+
+/* TranslationText
+ * Serializable representation of the data file. Has the serialVersionUID of 19L.
+ * Member data names should be based on those implied by the getters. Member data are not final.
+*/
+
+  /* getSentence()
+   * Getter method, returns String
+  */
+
+  /* getMonths()
+   * Getter method, returns String[]
+  */
+
+  /* getDays()
+   * Getter method, returns String[]
+  */
+
+  /* getMonth()
+   * Accepts an integer 0-11 corresponding to an index in the months array,
+   * and returns the value at that index. (e.g., 0 = January)
+  */
+
+  /* getDay()
+   * Accepts an integer 0-30 corresponding to an index in the day array,
+   * and returns the value at that index. (e.g., 30 = 31st)
+  */
+
+  /* Constructor
+   * Accepts a String array of months, a String array of days, and a String 
+   * containing a sentence with formatting.
+  */
+
